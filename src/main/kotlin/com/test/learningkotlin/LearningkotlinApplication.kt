1ee1d4c1d1999af73cb9bc4272d76fadd083c21a
unzereldrid <eldrid.aditya@unzer.com>
2022-03-11 16:57:44 +0100

    first commit

---
+++ src/main/kotlin/com/test/learningkotlin/LearningkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.test.learningkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningkotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningkotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/test/learningkotlin/LearningkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.learningkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningkotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
